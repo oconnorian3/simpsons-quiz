2c08783a709b7ac72ba4a6837ae467f5501355e0
oconnorian3 <oconnorian3@gmail.com>
2022-11-10 20:04:40 +0000

    Working on a JS Bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
 const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById ('question-container')
+const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById ('question')
 const answerButtonsElement = document.getElementById ('answers-buttons')
 
 let shuffledQuestions, currentQuestionIndex
 
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener('click', startQuiz)
 
 function startQuiz () {
-console.log('Started')
 startButton.classList.add('hide')
 shuffledQuestions = questions.sort(() => Math.random() - .5)
 currentQuestionIndex = 0
@@ -21,21 +20,21 @@ function displayNextQuestion () {
 }
 
 function showQuestion (question) {
-
+   questionElement.innerText = question.question
 }
 
 function selectAnswer () {
-  questionContainerElement.innerText = question.question
+  
 }
 
 const questions = [
     {
         question: 'What was the year the first episode of The Simpsons aired?',
         answers: [
-            {test: 1989, correct : true},
-            {test: 1987, correct : false},
-            {test: 1988, correct : false},
-            {test: 1990, correct : false},
+            { text: '1989', correct : true },
+            { text: '1987', correct : false },
+            { text: '1988', correct : false },
+            { text: '1990', correct : false }
         ]
     }
 ]

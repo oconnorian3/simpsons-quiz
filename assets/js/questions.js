672a2b372aca99af19a7c07d99da24f46160490d
oconnorian3 <oconnorian3@gmail.com>
2022-11-10 12:38:31 +0000

    Added link from landing page to quiz page and vice versa

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -9,13 +9,13 @@ const questions = [
     extra: 'The Simpsons first aired in 1989 and still makes new episodes to this day'
     },
     {
-    question: 'What's the name of the Simpsons pet dog?',
+    question: 'What is the name of the Simpsons pet dog?',
     choice1: 'Ruby',
     choice2: 'Ralph',
-    choice3: 'Santa's Little Helper',
+    choice3: 'Santas Little Helper',
     choice4: 'Snowy',
     answer: 3,
-    extra: 'Santa's Little Helper is the correct Answer'   
+    extra: 'Santas Little Helper is the correct Answer'   
     },
     {
     question: 'Which cartoon was the inspiration for The Itchy & Scratchy Show?',
@@ -27,7 +27,7 @@ const questions = [
     extra: 'The popular show Tom and Jerry inspired The Itchy & Scratchy Show'    
     },
     {
-    question: 'What is Marge's natural hair colour?',
+    question: 'What is Marges natural hair colour?',
     choice1: 'Grey',
     choice2: 'Blue',
     choice3: 'Brown',
@@ -36,16 +36,16 @@ const questions = [
     extra: 'The Answer is Grey, well done if you got that right.'
     },
     {
-    question: 'What's Homer's full time job?',
+    question: 'Whats Homers full time job?',
     choice1: 'Duffman',
     choice2: 'Chief of police',
     choice3: 'Bus Driver',
-    choice4: 'Nuclear Safety Inspector,
+    choice4: 'Nuclear Safety Inspector',
     answer: 4,   
     extra: 'Homer has had many jobs but Nuclear Safety Inspector would be his full time position'
     },
     {
-    question: 'What is Marge's maiden name?',
+    question: 'What is Marges maiden name?',
     choice1: 'Garnier',
     choice2: 'Fitzgerald',
     choice3: 'Never Revealed',
@@ -63,28 +63,28 @@ const questions = [
     extra: 'Apu and Manjula had octuplets'   
     },
     {
-    question: 'What are Marge's twin sisters called?',
+    question: 'What are Marges twin sisters called?',
     choice1: 'Maggie & Sheila',
     choice2: 'Tia & Tamera',
     choice3: 'Patty & Selma',
     choice4: ' Kristina & Karissa',
     answer: 3,   
-    extra: 'Patty & Selam of course.
+    extra: 'Patty & Selam of course.'
     },
     {
-    question: Who performed the role of Sideshow Bob in "The Simpsons" and was a former "Cheers" star?,
+    question: 'Who performed the role of Sideshow Bob in "The Simpsons" and was a former "Cheers" star?',
     choice1: 'Ted Danson',
     choice2: 'Kelsey Grammar',
     choice3: 'Woody Harreison',
     choice4: 'George Wendt',
     answer: 2,   
-    extra: 'Kelsey Grammar who also starred in 'Fraiser" is the correct answer
+    extra: 'Kelsey Grammar who also starred in "Fraiser" is the correct answer'
     },
     {
     question: 'Homer was part of a barbershop quartet. what was it called?',
     choice1: 'The Be Sharps',
     choice2: 'The Ce sharps',
-    choice3: 'The De Sharps,
+    choice3: 'The De Sharps',
     choice4: 'The Ee Sharps',
     answer: 1,   
     extra: 'Homer , Apu , Barney & Prncipal Skinner were all part of the Be Sharps'

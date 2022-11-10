38f9525689f6a5a95aa86e7ba3a7693b2f4fbab8
oconnorian3 <oconnorian3@gmail.com>
2022-11-10 16:59:12 +0000

    Removed and re-added Java blueprint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,30 @@
-function startQuiz ()  {
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById ("question-container")
+
+startButton.addEventListener("click", startGame)
+
+function startQuiz () {
+console.log('Started')
+startButton.classList.add('hide')
+questionContainerElement.classList.remove('hide')
+displayNextQuestion
+}
+
+function displayNextQuestion () {
 
 }
 
-function deployQuestion ()  {
+function selectAnswer () {
 
 }
 
-function displayquestion ()  {
-
-}
-
-function gameOver ()  {
-
-}
+const questions = [
+    [
+        question : 'What was the year the first episode of The Simpsons aired?'
+        answers: [
+            {test: 1989, correct : true}
+            {test: 1987, correct : false}
+            {test: 1988, correct : false}
+            {test: 1990, correct : false}
+        ]
+]

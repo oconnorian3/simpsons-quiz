4aee11c371f9ee8782681da3a3157f076c0387db
oconnorian3 <oconnorian3@gmail.com>
2022-11-17 21:09:02 +0000

    Tidied up and removed more uneeded code

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,22 +75,8 @@
    }
  }
  
- function setStatusClass(element, correct) {
-     clearStatusClass(element)
-     if (correct) {
-       element.classList.add('correct')
+ 
    
+
  
-     } else {
-       element.classList.add('wrong')
-     }
-   }
-   
-   function clearStatusClass(element) {
-     element.classList.remove('correct')
-     element.classList.remove('wrong')
-   }
- 
- // Increments the corects score
- 
-    
+
